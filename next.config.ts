498164d81f78d78eca212a3a99a8d4e267dd3986
Bad-F0x <128611760+Bad-F0x@users.noreply.github.com>
2026-07-13 00:24:10 +0600

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,11 @@ const basePath =
   process.env.BASE_PATH !== undefined ? process.env.BASE_PATH : "";
 
 const nextConfig: NextConfig = {
-  output: process.env.OUTPUT_MODE === "export" ? "export" : undefined,
-
   ...(basePath && {
     basePath,
     assetPrefix: basePath,
   }),
 
-  ...(process.env.OUTPUT_MODE === "export" && {
-    images: {
-      unoptimized: true,
-    },
-  }),
   allowedDevOrigins: ["*"],
 
   devIndicators: false,
